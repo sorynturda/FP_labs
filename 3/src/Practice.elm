873ea6b451988eb832371d45d3e29b78ccca02bb
Sorin Turda <s.turda12@gmail.com>
2024-10-20 20:00:41 +0300

    aproape gata tema lab 03

--- 3/src/Practice.elm
+++ 3/src/Practice.elm
@@ -1,4 +1,50 @@
 module Practice exposing (..)
 
 safeDiv : Int -> Int -> Maybe Int
-safeDiv a b = b
+safeDiv a b = 
+  if b == 0 then
+    Nothing
+  else
+    Just (a // b)
+
+len : List Int -> Int
+len lx = 
+  case lx of 
+    [] -> 0
+    x::xs -> 1 + len xs
+
+lenTail : List Int -> Int
+lenTail lx =
+  let
+    lenTailHelper ly a =
+      case ly of 
+        [] -> a
+        x::xs -> lenTailHelper xs (a + 1)
+
+  in
+    lenTailHelper lx 0
+
+last : List Int -> Maybe Int
+last lx = 
+  case lx of
+    [] -> Nothing
+    [x] -> Just x
+    x::xs -> last xs
+
+indexList : List Int -> Int -> Maybe Int
+indexList lx i = 
+  let
+    helper ly j cnt = 
+      case ly of
+        [] -> Nothing
+        x::xs -> 
+          if j == cnt then
+            Just x
+          else 
+            helper xs j (cnt + 1)    
+  in 
+    if i < 0 then
+      Nothing
+    else
+      helper lx i 0
+  
